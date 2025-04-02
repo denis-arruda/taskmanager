809c4cc149c3febca15155ace97e0800b8aa9662
Denis Arruda <denis.arruda@gmail.com>
2025-04-02 10:39:26 -0300

    Migrate from Spring to Quarkus

--- src/main/java/com/denisarruda/taskmanager/persistence/TaskRepository.java
+++ src/main/java/com/denisarruda/taskmanager/persistence/TaskRepository.java
@@ -1,9 +1,48 @@
 package com.denisarruda.taskmanager.persistence;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-public interface TaskRepository extends MongoRepository<TaskDocument, String> {
+import org.bson.Document;
 
-  // You can add custom query methods here if needed, for example:
-  // List<TaskDocument> findByStatus(Status status);
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.InsertOneResult;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class TaskRepository {
+
+  @Inject
+  MongoClient mongoClient;
+
+  public List<Document> findAll() {
+    return getCollection().find().into(new ArrayList<>());
+  }
+
+  public Optional<Document> findById(String id) {
+    Document taskDocument = getCollection().find(new Document("id", id)).first();
+    return Optional.ofNullable(taskDocument);
+  }
+
+  public String save(Document taskDocument) {
+    InsertOneResult result = getCollection().insertOne(taskDocument);
+    return result.getInsertedId().asString().getValue();
+  }
+
+  public void update(Document taskDocument) {
+    String id = taskDocument.getString("id");
+    getCollection().replaceOne(new Document("id", id), taskDocument);
+  }
+
+  private MongoCollection<Document> getCollection() {
+    return mongoClient.getDatabase("task").getCollection("task");
+  }
+
+  public void deleteById(String id) {
+    getCollection().deleteOne(new Document("id", id));
+  }
 }
--- src/test/java/com/denisarruda/taskmanager/controller/TaskResourceEndToEndTest.java
+++ src/test/java/com/denisarruda/taskmanager/controller/TaskResourceEndToEndTest.java
@@ -1,159 +1,162 @@
 package com.denisarruda.taskmanager.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
-import com.denisarruda.taskmanager.TestcontainersConfiguration;
 import com.denisarruda.taskmanager.domain.Status;
 import com.denisarruda.taskmanager.domain.Task;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+
+import io.restassured.http.ContentType;
+
+@QuarkusTest
 class TaskResourceEndToEndTest {
 
-  @LocalServerPort
-  private int port = 8080;
-
-  private TestRestTemplate restTemplate;
-
-  private String baseUrl;
-
-  @BeforeEach
-  void setUp() {
-    baseUrl = "http://localhost:" + port + "/api/tasks";
-    restTemplate = new TestRestTemplate();
-  }
+  @InjectMock
+  TaskManagerService taskManagerService;
 
   @Test
   void testCreateTask() throws Exception {
-    // Given
-    Task newTask = new Task(null, "Test Task", "Test Description", LocalDate.now(), Status.PENDING);
 
-    // When
-    ResponseEntity<Task> response = restTemplate.postForEntity(baseUrl, newTask, Task.class);
+    Task newTask = new Task("3632", "Test Task", "Test Description", LocalDate.now(), Status.PENDING);
 
-    // Then
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    Mockito.when(taskManagerService.createTask(Mockito.any(Task.class)))
+        .thenReturn(newTask);
 
-    Task createdTask = response.getBody();
-    assertThat(createdTask).isNotNull();
-    assertThat(createdTask.id()).isNotNull();
-    assertThat(createdTask.title()).isEqualTo("Test Task");
-    assertThat(createdTask.description()).isEqualTo("Test Description");
-    assertThat(createdTask.dueDate()).isEqualTo(LocalDate.now());
-    assertThat(createdTask.status()).isEqualTo(Status.PENDING);
+    given()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(newTask)
+        .when()
+        .post("/api/tasks")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.JSON)
+        .body(
+            "id", notNullValue(),
+            "title", is("Test Task"),
+            "description", is("Test Description"),
+            "dueDate", is(LocalDate.now().toString()),
+            "status", is(Status.PENDING.toString()));
+
+    Mockito.verify(taskManagerService).createTask(Mockito.any(Task.class));
+    Mockito.verifyNoMoreInteractions(this.taskManagerService);
   }
 
   @Test
   void testGetTaskById() throws Exception {
     // 1. Create a task first to get a valid ID
-    Task newTask = new Task(null, "Task for GET", "Description", LocalDate.now(), Status.PENDING);
-    ResponseEntity<Task> createdTaskResponse = restTemplate.postForEntity(baseUrl, newTask, Task.class);
-    assertThat(createdTaskResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-    String createdTaskId = createdTaskResponse.getBody().id();
+    Task newTask = new Task("6033", "Task for GET", "Description", LocalDate.now(), Status.PENDING);
 
-    // 2. Now retrieve the task using the generated ID
-    ResponseEntity<Task> response = restTemplate.getForEntity(baseUrl + "/" + createdTaskId, Task.class);
+    Mockito.when(taskManagerService.getTask(Mockito.eq(newTask.id())))
+        .thenReturn(newTask);
 
-    // Then
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    Task retrievedTask = response.getBody();
-    assertThat(retrievedTask).isNotNull();
-    assertThat(retrievedTask.id()).isEqualTo(createdTaskId);
+    given()
+        .when()
+        .get("/api/tasks/" + newTask.id())
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.JSON)
+        .body(
+            "id", is(newTask.id()),
+            "title", is(newTask.title()),
+            "description", is(newTask.description()),
+            "dueDate", is(LocalDate.now().toString()),
+            "status", is(Status.PENDING.toString()));
+
+    Mockito.verify(taskManagerService).getTask(Mockito.eq(newTask.id()));
+    Mockito.verifyNoMoreInteractions(this.taskManagerService);
   }
 
   @Test
   void testUpdateTask() throws Exception {
     // 1. Create a task to update
-    Task newTask = new Task(null, "Task to Update", "Description", LocalDate.now(), Status.PENDING);
-    ResponseEntity<Task> createdTaskResponse = restTemplate.postForEntity(baseUrl, newTask, Task.class);
-    assertThat(createdTaskResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-    Task createdTask = createdTaskResponse.getBody();
+    Task newTask = new Task("8234", "Task to Update", "Description", LocalDate.now(), Status.IN_PROGRESS);
 
-    // 2. Update the task
-    Task updatedTask = new Task(createdTask.id(), "Updated Task", "Updated Description",
-        LocalDate.now().plusDays(1), Status.IN_PROGRESS);
+    Mockito.when(taskManagerService.updateTask(Mockito.any(Task.class)))
+        .thenReturn(newTask);
 
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    HttpEntity<Task> requestEntity = new HttpEntity<>(updatedTask, headers);
+    given()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(newTask)
+        .when()
+        .put("/api/tasks")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.JSON)
+        .body(
+            "id", is(newTask.id()),
+            "title", is(newTask.title()),
+            "description", is(newTask.description()),
+            "dueDate", is(LocalDate.now().toString()),
+            "status", is(Status.IN_PROGRESS.toString()));
 
-    ResponseEntity<Task> response = restTemplate.exchange(baseUrl, org.springframework.http.HttpMethod.PUT,
-        requestEntity, Task.class);
-
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    Task updatedTaskFromResponse = response.getBody();
-    assertThat(updatedTaskFromResponse).isNotNull();
-    assertThat(updatedTaskFromResponse.id()).isEqualTo(createdTask.id());
-    assertThat(updatedTaskFromResponse.title()).isEqualTo("Updated Task");
-    assertThat(updatedTaskFromResponse.description()).isEqualTo("Updated Description");
-    assertThat(updatedTaskFromResponse.dueDate()).isEqualTo(LocalDate.now().plusDays(1));
-    assertThat(updatedTaskFromResponse.status()).isEqualTo(Status.IN_PROGRESS);
+    Mockito.verify(taskManagerService).updateTask(Mockito.any(Task.class));
+    Mockito.verifyNoMoreInteractions(this.taskManagerService);
   }
 
   @Test
   void testDeleteTask() throws Exception {
-    // 1. Create a task to delete
-    Task newTask = new Task(null, "Task to Delete", "Description", LocalDate.now(), Status.PENDING);
-    ResponseEntity<Task> createdTaskResponse = restTemplate.postForEntity(baseUrl, newTask, Task.class);
-    assertThat(createdTaskResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-    String createdTaskId = createdTaskResponse.getBody().id();
 
-    // 2. Delete the task
-    restTemplate.delete(baseUrl + "/" + createdTaskId);
+    given()
+        .when()
+        .delete("/api/tasks/11726")
+        .then()
+        .statusCode(HttpStatus.NO_CONTENT.value());
 
-    // 3. Verify that the task is no longer available
-    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/" + createdTaskId, String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    Mockito.verify(taskManagerService).deleteTask(Mockito.eq("11726"));
+    Mockito.verifyNoMoreInteractions(this.taskManagerService);
   }
 
   @Test
   void testGetTasksByStatusAndDueDate() throws Exception {
     // 1. Create some tasks with known statuses and due dates
-    Task task1 = new Task(null, "Task 1", "Description 1", LocalDate.now(), Status.PENDING);
-    Task task2 = new Task(null, "Task 2", "Description 2", LocalDate.now().plusDays(1), Status.IN_PROGRESS);
-    Task task3 = new Task(null, "Task 3", "Description 3", LocalDate.now(), Status.COMPLETED);
+    Task task1 = new Task("12930", "Task 1", "Description 1", LocalDate.now().plusDays(1), Status.PENDING);
+    Task task2 = new Task("13032", "Task 2", "Description 2", LocalDate.now().plusDays(2), Status.PENDING);
+    Task task3 = new Task("13131", "Task 3", "Description 3", LocalDate.now().plusDays(3), Status.PENDING);
 
-    restTemplate.postForEntity(baseUrl, task1, Task.class);
-    restTemplate.postForEntity(baseUrl, task2, Task.class);
-    restTemplate.postForEntity(baseUrl, task3, Task.class);
+    Mockito
+        .when(taskManagerService.getTasksByStatusAndDueDate(Mockito.eq(Status.PENDING), Mockito.isNull(),
+            Mockito.eq("dueDate"), Mockito.eq("asc")))
+        .thenReturn(Arrays.asList(task1, task2, task3));
 
-    // 2. Test filtering by status
-    ResponseEntity<Task[]> responseByStatus = restTemplate.getForEntity(baseUrl + "?status=PENDING",
-        Task[].class);
-    assertThat(responseByStatus.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseByStatus.getBody()).hasSize(1);
-    assertThat(responseByStatus.getBody()[0].title()).isEqualTo("Task 1");
+    given()
+        .when()
+        .get("/api/tasks?status=PENDING&sortBy=dueDate&sortOrder=asc")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.JSON)
+        .body(
+            "[0].id", is(task1.id()),
+            "[0].title", is(task1.title()),
+            "[0].description", is(task1.description()),
+            "[0].dueDate", is(task1.dueDate().toString()),
+            "[0].status", is(Status.PENDING.toString()),
 
-    // 3. Test filtering by dueDate
-    ResponseEntity<Task[]> responseByDueDate = restTemplate.getForEntity(
-        baseUrl + "?dueDate=" + LocalDate.now(),
-        Task[].class);
-    assertThat(responseByDueDate.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseByDueDate.getBody()).hasSize(2);
+            "[1].id", is(task2.id()),
+            "[1].title", is(task2.title()),
+            "[1].description", is(task2.description()),
+            "[1].dueDate", is(task2.dueDate().toString()),
+            "[1].status", is(Status.PENDING.toString()),
 
-    // 4. Test sorting - This is a bit more complex with TestRestTemplate,
-    // you might need to inspect the response headers or rely on the order being
-    // consistent for now.
-    ResponseEntity<Task[]> responseSortedByDate = restTemplate
-        .getForEntity(baseUrl + "?sortBy=dueDate&sortOrder=desc", Task[].class);
-    assertThat(responseSortedByDate.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseSortedByDate.getBody()).hasSize(3);
-    assertThat(responseSortedByDate.getBody()[0].title()).isEqualTo("Task 2");
-    assertThat(responseSortedByDate.getBody()[1].title()).isEqualTo("Task 1");
-    assertThat(responseSortedByDate.getBody()[2].title()).isEqualTo("Task 3");
+            "[2].id", is(task3.id()),
+            "[2].title", is(task3.title()),
+            "[2].description", is(task3.description()),
+            "[2].dueDate", is(task3.dueDate().toString()),
+            "[2].status", is(Status.PENDING.toString()));
+
+    Mockito.verify(taskManagerService).getTasksByStatusAndDueDate(Mockito.eq(Status.PENDING), Mockito.isNull(),
+        Mockito.eq("dueDate"), Mockito.eq("asc"));
+    Mockito.verifyNoMoreInteractions(this.taskManagerService);
   }
 }
