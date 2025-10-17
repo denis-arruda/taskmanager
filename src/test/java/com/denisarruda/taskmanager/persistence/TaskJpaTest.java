db0aa52849d1c4fa0c574a4e204abe94a197582d
Denis Arruda Santos <denis.arruda@gmail.com>
2025-10-17 13:22:22 -0300

    Use only junit5

--- src/test/java/com/denisarruda/taskmanager/persistence/TaskJpaTest.java
+++ src/test/java/com/denisarruda/taskmanager/persistence/TaskJpaTest.java
@@ -1,6 +1,6 @@
 package com.denisarruda.taskmanager.persistence;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
